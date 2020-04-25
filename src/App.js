542c10046f65509b0c503f98d76eda4739e7b574
Andrea Marie Festøy <andrea@gojust.com>
2020-04-25 16:26:06 +0200

    Test api call with gif api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import "./App.css";
+import request from "superagent";
 
 const App = () => {
   const [inputField, setInputField] = useState();
+  const [gif, setGif] = useState();
   const onInput = (ev) => {
     const value = ev.target.value;
     setInputField(value);
@@ -10,6 +12,12 @@ const App = () => {
 
   const onClickSearch = () => {
     // use inputField value to search
+    const url = `http://api.giphy.com/v1/gifs/search?q=${inputField}&api_key=dc6zaTOxFJmzC`;
+    request.get(url, (err, res) => {
+      console.log(res.body.data);
+      const index = Math.floor(Math.random() * res.body.data.length);
+      setGif(res.body.data[index]);
+    });
   };
 
   return (
@@ -17,6 +25,7 @@ const App = () => {
       <h1>Bringing Swag Back To Housing</h1>
       <input value={inputField} onChange={onInput} />
       <button onClick={onClickSearch}>Search for address</button>
+      {gif && <img src={gif.images.downsized_large.url} />}
     </div>
   );
 };
